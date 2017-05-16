011628653ba2cf0bd18e5d3636cb91431d5bb9e8
arturomonriv <arturomonriv@hotmail.com>
2017-05-16 14:41:30 -0500

    Movie repositories

---
+++ WebAppCompositionRoot/MyTypes/IMovieRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCompositionRoot.MyTypes
+{
+    public interface IMovieRepository
+    {
+        IReadOnlyList<Movie> GetList();
+    }
+}
---
+++ WebAppCompositionRoot/MyTypes/MovieRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCompositionRoot.MyTypes
+{
+    public class MovieRepository : IMovieRepository
+    {
+        public IReadOnlyList<Movie> GetList()
+        {
+            Movie[] movies = new Movie[3];
+            Movie movie = new Movie();
+            movie.Id = 1;
+            movie.Name = "Santos contra las momias";
+            movies[0] = movie;
+
+            movie = new Movie();
+            movie.Id = 2;
+            movie.Name = "Biruta y Capulina";
+            movies[1] = movie;
+
+            movie = new Movie();
+            movie.Id = 3;
+            movie.Name = "Chabelo y Pepito contra los monstruos";
+            movies[2] = movie;
+
+            return movies;
+        }
+    }
+}
